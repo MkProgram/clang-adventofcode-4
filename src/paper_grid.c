3be5bf03e4c651cd1864f21f69c94b5d9d6da860
Marian <marian.kassel@outlook.de>
2026-08-30 14:16:41 +0200

    Initialize Paper grid

--- src/paper_grid.c
+++ src/paper_grid.c
@@ -1,3 +1,12 @@
 #include "paper_grid.h"
 
-int process_from_file(FILE *file) { return 0; }
+int process_from_file(FILE *file) {
+
+  char *line = NULL;
+
+  if (fgets(line, 200, file) == NULL) {
+    return 1;
+  }
+
+  return 0;
+}
